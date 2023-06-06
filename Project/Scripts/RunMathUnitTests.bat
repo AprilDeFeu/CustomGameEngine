6607da0454cc7b3289105bf7a88aa18429b28c6e
asolo <april.solomon@cae.com>
2023-06-06 19:45:13 -0400

    Removed unused scripts

--- Project/Scripts/RunMathUnitTests.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-cd ..\Src
-g++ Math\Vectors.cpp Math\Geometry.cpp Math\Matrices.cpp UnitTest\UnitTests.cpp Main\Main.cpp -o ..\Test\MathUnitTest.exe
-cd ..\Test
-start "" c:\Sources\RenderingEngine\Project\Test\MathUnitTest.exe
